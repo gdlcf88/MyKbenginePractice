c5ab8b78092c7b0898211af7e5db59936fa983b0
Super <47396430@qq.com>
2018-09-03 14:34:50 +0800

    commit

---
+++ scripts/common/KBEDebug.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import sys
+try:
+    import KBEngine
+except ImportError as e:
+    from KBEngineTips.BaseApp import KBEngine
+
+def printMsg(args, isPrintPath):
+	for m in args:print (m)
+
+def TRACE_MSG(*args):
+	KBEngine.scriptLogType(KBEngine.LOG_TYPE_NORMAL)
+	printMsg(args, False)
+
+def DEBUG_MSG(*args):
+	if KBEngine.publish() == 0:
+		KBEngine.scriptLogType(KBEngine.LOG_TYPE_DBG)
+		printMsg(args, True)
+
+def INFO_MSG(*args):
+	if KBEngine.publish() <= 1:
+		KBEngine.scriptLogType(KBEngine.LOG_TYPE_INFO)
+		printMsg(args, False)
+
+def WARNING_MSG(*args):
+	KBEngine.scriptLogType(KBEngine.LOG_TYPE_WAR)
+	printMsg(args, True)
+
+def ERROR_MSG(*args):
+	KBEngine.scriptLogType(KBEngine.LOG_TYPE_ERR)
+	printMsg(args, True)
